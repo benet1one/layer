581e6824e1ba66e3c2d72255159ad57869bba764
benet1one <benet1one@gmail.com>
2024-11-24 15:23:36 +0100

    fixed imports

--- R/package.R
+++ R/package.R
@@ -2,4 +2,8 @@
 #' @import rlang
 #' @import purrr
 #' @import pracma
-#' @rawNamespace import(magrittr, except = set_names) 
+#' @rawNamespace import(magrittr, except = set_names)
+
+hello_layer <- function() {
+    cat("i'm a secret function. hi.")
+}
